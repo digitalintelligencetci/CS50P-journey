42c213f83bd326ff04668d045e63ce0180b31574
Ordemia Hashim <219929616+digitalintelligencetci@users.noreply.github.com>
2026-05-18 19:52:13 +0100

    Add file-extensions.py — file extension to MIME type mapper

---
+++ problem-sets/week1-file-extensions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+file_name = input("Enter filename: ").lower()
+
+if file_name.endswith('.gif'):
+    print('image/gif')
+elif file_name.endswith('.jpg') or file_name.endswith('jpeg'):
+    print('image/jpeg')
+elif file_name.endswith('.png'):
+    print('image/png')
+elif file_name.endswith('.txt'):
+    print('text/plain')
+elif file_name.endswith('zip'):
+    print('application/zip')
+else:
+    print('application/octet-stream')
