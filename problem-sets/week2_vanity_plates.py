3e3ed3a922e477e3c68bfb85e81d013992ef4685
HasheemyO_ <219929616+digitalintelligencetci@users.noreply.github.com>
2026-07-30 13:46:40 +0100

    Week 2 Vanity Plate Problem set completed.

---
+++ problem-sets/week2_vanity_plates.py
@@ -0,0 +1,48 @@
+# Ask the user for a plate
+
+# Check whether:
+#   its length is between 2 and 6
+#   its first two characters are letters - not method
+#   it contains only letters and numbers
+#   any numbers appear only at the end
+#   the first number is not 0
+
+# If every check passes:
+#   return True
+# Otherwise:
+#   return False
+
+
+def main():
+    plate = input("Plate: ")                    # gets the user input
+    if is_valid(plate):                         # plate is now passed into is_valid
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):                                # special set of instructions of what to do with the input.
+    if len(s) < 2 or len(s) > 6:                # checks if the length is between 2 and 6.
+        return False                            # returns false if it does not meet the required condition.
+
+    if not s[:2].isalpha():                     # checks the first two characters are letters.
+                                                # not is used as a reverse method or if something must be true the code should read "if it is not what is true".
+        return False                           
+
+    if not s.isalnum():                         # checks it contains only letters and numbers.
+        return False
+
+    for i in range(len(s)):
+        if s[i].isnumeric() and s[i] == "0":    # = assigns a value and == compares two values
+            return False
+
+        if not s[i:].isnumeric():
+            return False
+        else:
+            break
+                                                # break stops the loop
+                                                # return false stops the entire function and sends back false.
+    return True
+
+
+main()
