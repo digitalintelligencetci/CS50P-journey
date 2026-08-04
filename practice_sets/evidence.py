3fb9a69d899a3fc86e4bb356679793b859275850
Ordemia Hashim <219929616+digitalintelligencetci@users.noreply.github.com>
2026-08-04 18:24:46 +0100

    Evidence.py pseudo code notes

---
+++ practice_sets/evidence.py
@@ -0,0 +1,9 @@
+# Pseudo Code:
+#   Store at least six evidence references and their corresponding descriptions in a dictionary.
+
+#   Ask the user how many searches they want to perform.
+#       Keep asking until the user enters a positive whole number.
+
+#   Repeat the search process while the number of completed searches is less than the number of requested searches.
+#       while completed searches < requested searches
+
